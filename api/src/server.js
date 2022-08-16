66230ec949fb8aecefc11d0b9c374262fdfa98c9
Jonatas Amorim Silva <jonatas231997@live.com>
2022-08-15 21:18:36 -0300

    Query params

--- api/src/server.js
+++ api/src/server.js
@@ -2,9 +2,17 @@ const express = require('express');
 
 const app = express();
 
-app.get("/message/:id/:user", (request, response) =>{
-    response.send(`Mensagem ID: ${request.params.id}. Para o usuário: ${request.params.user}.`);
+
+app.get("/message/:id/:user", (request, response) => {
+  const { id, user } = request.params;
+  response.send(`Mensagem ID: ${id}. Para o usuário: ${user}.`);
 })
+
+app.get("/users", (request, response) => {
+  const { page, limit } = request.query
+  response.send(`Página: ${page}. Monstar: ${limit}`);
+});
+
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
