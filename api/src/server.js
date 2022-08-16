a38f3c371aca89acd641370563e7e59c8122dea4
Jonatas Amorim Silva <jonatas231997@live.com>
2022-08-15 21:54:09 -0300

    nodemon

--- api/src/server.js
+++ api/src/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get("/message/:id/:user", (request, response) => {
   const { id, user } = request.params;
-  response.send(`Mensagem ID: ${id}. Para o usuário: ${user}.`);
+  response.send(`ID do usuário: ${id}. Nome do usuário: ${user}.`);
 })
 
 app.get("/users", (request, response) => {
