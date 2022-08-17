01fcc2637d73ebf0f928a7a9c22d67abcbc9538f
Jonatas_Amorim <jonatas231997@live.com>
2022-08-17 17:29:34 -0300

    Users Controller

--- api/src/controllers/UsersController.js
+++ api/src/controllers/UsersController.js
@@ -1,13 +1,4 @@
 class UsersController {
-    /* 
-    Metodos
-    index - GET para listar varios registros.
-    show - GET para exibir um registro especifico.
-    create - POST para criar um registro.
-    update - PUT para atualizar um registro.
-    delete - DELETE para remover um registro.
-     */
-
     create(request, response) {
         const { name, email, password } = request.body;
         /* response.send(`Usuário: ${name} E-mail: ${email} Senha: ${password}` ); */
@@ -15,4 +6,4 @@ class UsersController {
     }
 }
 
-module.export = UsersController
+module.exports = UsersController;
