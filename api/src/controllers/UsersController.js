4a9436a4f172fbd6a25e8bfe2137094ef8c66b4d
Jonatas Amorim Silva <jonatas231997@live.com>
2022-08-17 21:32:11 -0300

    Status Code

--- api/src/controllers/UsersController.js
+++ api/src/controllers/UsersController.js
@@ -2,7 +2,7 @@ class UsersController {
     create(request, response) {
         const { name, email, password } = request.body;
         /* response.send(`Usuário: ${name} E-mail: ${email} Senha: ${password}` ); */
-        response.json({ name, email, password });
+        response.status(201).json({ name, email, password });
     }
 }
 
